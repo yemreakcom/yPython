b02c62fb020ee0471de20244269766c6758b773a
Yunus Emre <49655146+yedhrab@users.noreply.github.com>
2019-10-11 11:42:31 +0300

    Update integration.sh

--- integration.sh
+++ integration.sh
@@ -1,4 +1,4 @@
-python ../YGitBookIntegration/integrate.py .
+python ../YGitBookIntegration/integrate.py . -ll 1
 echo "---
 description: Sitede neler olup bittiğinin raporudur.
 ---" > CHANGELOG.md
